e9f183fcf70996af5751ad0efa890bca99dbe6f0
Pee Tankulrat <8246751+PiiTan@users.noreply.github.com>
2024-01-23 09:29:16 +0700

    Setup iconify for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@ import tailwind from "@astrojs/tailwind";
 import { defineConfig } from 'astro/config';
 
 import react from "@astrojs/react";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react(), icon()]
 });
